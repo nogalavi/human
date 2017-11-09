5cf3755a6448fc2e2646383c7b5629f42ba38df4
nogalavi <noga@indeni.com>
2017-11-09 11:19:18 +0200

    paypal integration

--- src/App.js
+++ src/App.js
@@ -11,7 +11,13 @@ class App extends Component {
           <h1 className="App-title">היוש מיקי</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> היוש היוש.
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <input type="hidden" name="cmd" value="_s-xclick"/>
+                    <input type="hidden" name="hosted_button_id" value="W8JU8BMDLBQCA"/>
+                        <input type="image" src="http://singles.mako.co.il/uploads/full/blog_26.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+                            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+            </form>
+
         </p>
       </div>
     );
